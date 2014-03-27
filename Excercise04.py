cf5480f35dd2a76832e494b1113ca2a451e300bf
Schachte <rschacht@asu.edu>
2014-03-26 23:39:39 -0700

    Further Testing
    GitHub Test 2

--- Excercise04.py
+++ Excercise04.py
@@ -10,9 +10,11 @@ available_cars = 30
 number_of_people = 12
 number_of_cars_used = number_of_people
 cars_left_over = available_cars - number_of_people
+git_hub_test = "test"
 
 print "There are",available_cars,"cars available." #String and variable concatenation
 print "There are",number_of_people,"people willing to be drivers." #String and variable concatenation
 print "Once all the drivers started driving, there were",cars_left_over,"cars left." #String and variable concatenation
 
 #Edit Test GITHUB
+#More GITHUB Testing
