ce18609fa199a75580b0ac7ea67756b6d54542ee
Schachte <rschacht@asu.edu>
2014-04-26 17:44:37 -0700

    Exercise 01-21 Summary Review
    Reviewing all the previously written exercises and explaining their
    meaning along with some of their functions and how they work.

---
+++ Excercise22.py
@@ -0,0 +1,103 @@
+################################
+######### Excercise 22##########
+# Learning Python The Hard Way#
+####### Ryan Schachte ##########
+################################
+
+
+'''
+This is a file to review and explain everything studied so far between ex01 - ex21
+'''
+
+#Excercise 1:
+#Print function allows you to display string data
+
+#Excercise 2:
+#Using comments like the one's shown here with the '#' sign or triple quotes
+
+#Excercise 3:
+#Use comparison operators and outputting the value it returns
+#   i.e (print 5 > 0) would print the value 'True'
+
+#Excercise 4:
+#Excercise focuses on assigning string or integer data to a variable and then implementing that variable into a print statement
+
+#Excercise 5:
+#Uses string formatting techniques to print data to make it easier to print data entered by the user
+#i.e (Your name is %s) %name would print Your name is Josh
+
+#Excercise 6:
+#Showing that you can combine string data using the plus operator to create a concatentated print statement
+
+#Excercise 7:
+#Showing you can multiple a number by a string and it will create that string x amount of times
+#Showing you can concatenate character by character and print the line with the plus operator to show that the characters were combined into single words
+
+#Excercise 8:
+#Integers and booleans do not contain quotes when using string formatting
+
+#Excercise 9:
+#Using basic escape sequences in python to display data in print statements
+#i.e \n would take the string and then start a new line where that escape sequence was entered
+
+#Excercise 10:
+#Uses more escape sequences
+#\t = tabbed indent, \n = new line, \\ = one backslash in print statement, which can also be used to insert quotes into a print statement
+
+#Excercise 11:
+#Uses casting, concatentation, string formatting and raw input to make a functional program from user input
+#raw_input will allow user to enter information
+#Allow info into raw_input is a string, so you can cast that string to other types ie int(var_name) or float(var_name)
+
+#Excercise 12:
+#Raw input with string formatting
+#Note: For string formatting into a raw input string, you must add the modulo operator inside of the raw_input parenthesis
+
+#Excercise 13:
+#Programming allows more than 1 argument to be passed in the terminal. These arguments are used like variables throughout the program
+# from sys import argv
+# script_name, argument1, argument2 = argv
+
+#Excercise 14:
+#Reading and writing to files in Python
+#   Open file:
+#       file_open = open(filename, "w")
+#   Close file:
+#       file_open.close()
+#   Read file:
+#       print file_open.read()
+#
+#You can also print the bytes contained in the file using...
+#       print len(file_open)
+
+#Excercise 15:
+#Using raw_input and conditional to make a functional reading and writing program
+#Write to a file using:
+#                       file_open = open(filename, "w")
+#                       file_open.write(DATA_TO_WRITE_HERE)
+
+#Excercise 16:
+#Check if a file exists using:
+#   from os.path import exists
+
+#Excercise 17:
+#print exists(FILE) >> would return a boolean inside of the print statement
+#More basis file writing functions used in the previous excercise
+
+#Excercise 18:
+#Passing defined arguments into parameters of functions, concatenating them into a string and returning the values
+
+#Excercise 19:
+#Conditionals, functions and returns to keep track of integer counts, etc.
+
+#Excerse 20:
+#Defining functions to open, close and read files. (Combinations of function excercise with file writing excercise)
+
+#Excercise 21:
+#String concatenation using arguments, functions, returns and string formatting techniques
+
+#Excercise 22:
+#Fin
+
+
+
